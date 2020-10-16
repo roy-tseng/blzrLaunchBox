db41300980255ee6852378c06a5b23b15ba8e2f3
roy <roy@infolink.com.tw>
2020-10-16 15:05:18 +0800

    the 1st version

---
+++ Shared/StoreString.cs
@@ -0,0 +1,14 @@
+namespace blzrlunch.Shared
+{
+    public static class StoreString
+    {
+        public const string Title = "店名";
+        
+        public const string Address = "地址";
+
+        public const string Introduction = "介紹";
+
+        public const string VisitTimes = "次數";
+    }
+
+}
